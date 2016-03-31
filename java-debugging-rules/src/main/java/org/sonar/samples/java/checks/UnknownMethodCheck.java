f867b66d10d92176d990657236f77afcec9960ea
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-03-31 18:11:47 +0200

    raise issue also on unknown constructors

--- java-debugging-rules/src/main/java/org/sonar/samples/java/checks/UnknownMethodCheck.java
+++ java-debugging-rules/src/main/java/org/sonar/samples/java/checks/UnknownMethodCheck.java
@@ -6,11 +6,13 @@ import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Rule;
 import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
 import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.JavaFileScannerContext.Location;
+import org.sonar.plugins.java.api.tree.Arguments;
 import org.sonar.plugins.java.api.tree.ExpressionTree;
 import org.sonar.plugins.java.api.tree.MemberSelectExpressionTree;
 import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.NewClassTree;
 import org.sonar.plugins.java.api.tree.Tree;
-import org.sonar.plugins.java.api.tree.Tree.Kind;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 
@@ -26,26 +28,48 @@ import java.util.List;
 public class UnknownMethodCheck extends IssuableSubscriptionVisitor {
 
   @Override
-  public List<Kind> nodesToVisit() {
-    return ImmutableList.of(Tree.Kind.METHOD_INVOCATION);
+  public List<Tree.Kind> nodesToVisit() {
+    return ImmutableList.of(Tree.Kind.METHOD_INVOCATION, Tree.Kind.NEW_CLASS);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    MethodInvocationTree mit = (MethodInvocationTree) tree;
-    if (mit.symbol().isUnknown()) {
-      Tree reportTree = mit.methodSelect();
-      List<JavaFileScannerContext.Location> secondary = new ArrayList<>();
-      for (ExpressionTree argument : mit.arguments()) {
-        if (argument.symbolType().isUnknown()) {
-          secondary.add(new JavaFileScannerContext.Location("Unknown type", argument));
-        }
-      }
-      if (reportTree.is(Tree.Kind.MEMBER_SELECT)) {
-        reportTree = ((MemberSelectExpressionTree) reportTree).identifier();
-      }
-      reportIssue(reportTree, "Unknown method", secondary, null);
+    if (tree.is(Tree.Kind.METHOD_INVOCATION)) {
+      handleMethodInvocation((MethodInvocationTree) tree);
+    } else {
+      handleConstructor((NewClassTree) tree);
     }
+
+  }
+
+  private void handleConstructor(NewClassTree tree) {
+    if (tree.constructorSymbol().isUnknown()) {
+      reportUnknownMethod(tree.identifier(), "Unknown constructor", getMissingArguments(tree.arguments()));
+    }
+  }
+
+  private void handleMethodInvocation(MethodInvocationTree tree) {
+    if (tree.symbol().isUnknown()) {
+      reportUnknownMethod(tree.methodSelect(), "Unknown method", getMissingArguments(tree.arguments()));
+    }
+  }
+
+  private static List<Location> getMissingArguments(Arguments arguments) {
+    List<JavaFileScannerContext.Location> secondaries = new ArrayList<>();
+    for (ExpressionTree argument : arguments) {
+      if (argument.symbolType().isUnknown()) {
+        secondaries.add(new JavaFileScannerContext.Location("Unknown type", argument));
+      }
+    }
+    return secondaries;
+  }
+
+  void reportUnknownMethod(Tree reportTree, String message, List<JavaFileScannerContext.Location> secondaries) {
+    Tree target = reportTree;
+    if (target.is(Tree.Kind.MEMBER_SELECT)) {
+      target = ((MemberSelectExpressionTree) target).identifier();
+    }
+    reportIssue(target, message, secondaries, null);
   }
 
 }
--- java-debugging-rules/src/test/files/UnknownMethodCheck.java
+++ java-debugging-rules/src/test/files/UnknownMethodCheck.java
@@ -1,13 +1,24 @@
+package org.foo;
+
 class A {
+  A(String s, int i) {}
+
   void foo() {
+    new A("hello", 0); // Compliant
+    new A( // Noncompliant [[sc=9;ec=10;secondary=9]] {{Unknown constructor}}
+      unknownVar
+      , 1);
+    new org.foo.A(); // noncompliant [[sc=17;ec=18]] {{Unknown constructor}}
+
     foo(); // Compliant
-    bar(); // Noncompliant [[sc=5;ec=8]]
+    gul("hello", 0); // Compliant
+
+    bar(); // Noncompliant [[sc=5;ec=8]] {{Unknown method}}
     Unknown.bar(); // Noncompliant [[sc=13;ec=16]]
     this.bar(); // Noncompliant [[sc=10;ec=13]]
-    gul("hello", 0); // Compliant
-    gul( // Noncompliant [[sc=5;ec=8;secondary=9]]
-      unknownVar,
-      1);
+    gul( // Noncompliant [[sc=5;ec=8;secondary=20]]
+      unknownVar
+      , 1);
   }
 
   void gul(String s, int i) {}
