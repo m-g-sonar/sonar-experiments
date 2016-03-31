f21f3fb59191b40be48f0eeffa2d06bcbf370015
Michael Gumowski <michael.gumowski@sonarsource.com>
2016-03-31 18:25:06 +0200

    split into 2 checks for constructor and methods

--- java-debugging-rules/src/main/java/org/sonar/samples/java/checks/UnknownConstructorCheck.java
+++ java-debugging-rules/src/main/java/org/sonar/samples/java/checks/UnknownConstructorCheck.java
@@ -4,31 +4,31 @@ import com.google.common.collect.ImmutableList;
 
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Rule;
-import org.sonar.plugins.java.api.tree.MethodInvocationTree;
+import org.sonar.plugins.java.api.tree.NewClassTree;
 import org.sonar.plugins.java.api.tree.Tree;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
 
 import java.util.List;
 
-@Rule(key = "UnknownMethodInvocation",
-  name = "Unknown Method invocation",
-  description = "This rule detects invocation of unknown method (not resolved)",
+@Rule(key = "UnknownConstructorCall",
+  name = "Unknown Constructor Call",
+  description = "This rule detects unknown constructors (not resolved)",
   tags = {"debugging"})
 @SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.RELIABILITY_COMPLIANCE)
 @SqaleConstantRemediation("10min")
-public class UnknownMethodCheck extends UnknownTypeAbstractCheck {
+public class UnknownConstructorCheck extends UnknownTypeAbstractCheck {
 
   @Override
   public List<Tree.Kind> nodesToVisit() {
-    return ImmutableList.of(Tree.Kind.METHOD_INVOCATION);
+    return ImmutableList.of(Tree.Kind.NEW_CLASS);
   }
 
   @Override
   public void visitNode(Tree tree) {
-    MethodInvocationTree mit = (MethodInvocationTree) tree;
-    if (mit.symbol().isUnknown()) {
-      reportUnknownType(mit.methodSelect(), "Unknown method", getMissingArguments(mit.arguments()));
+    NewClassTree nct = (NewClassTree) tree;
+    if (nct.constructorSymbol().isUnknown()) {
+      reportUnknownType(nct.identifier(), "Unknown constructor", getMissingArguments(nct.arguments()));
     }
   }
 
--- java-debugging-rules/src/test/files/UnknownMethodCheck.java
+++ java-debugging-rules/src/test/files/UnknownMethodCheck.java
@@ -1,22 +1,14 @@
 package org.foo;
 
 class A {
-  A(String s, int i) {}
-
   void foo() {
-    new A("hello", 0); // Compliant
-    new A( // Noncompliant [[sc=9;ec=10;secondary=9]] {{Unknown constructor}}
-      unknownVar
-      , 1);
-    new org.foo.A(); // noncompliant [[sc=17;ec=18]] {{Unknown constructor}}
-
     foo(); // Compliant
     gul("hello", 0); // Compliant
 
     bar(); // Noncompliant [[sc=5;ec=8]] {{Unknown method}}
     Unknown.bar(); // Noncompliant [[sc=13;ec=16]]
     this.bar(); // Noncompliant [[sc=10;ec=13]]
-    gul( // Noncompliant [[sc=5;ec=8;secondary=20]]
+    gul( // Noncompliant [[sc=5;ec=8;secondary=12]]
       unknownVar
       , 1);
   }
