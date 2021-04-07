cd4eb83d95992f073ca7935ca919d37039c925f0
Michael Gumowski <michael.gumowski@sonarsource.com>
2021-04-07 16:11:59 +0200

    java 16 support

--- HeadShot-java11/src/main/java/org/foo/A.java
+++ HeadShot-java11/src/main/java/org/foo/A.java
@@ -5,9 +5,11 @@ import java.util.regex.Pattern;
 public class A {
 
   public static void main(String[] args) {
-    String regex = "[^a-c]+";
-    Pattern pattern = Pattern.compile(regex);
-    System.out.println(pattern.matcher("def").matches());
-    System.out.println(pattern.matcher("abc").matches());
+    Pattern p = Pattern.compile("""
+      [
+
+      ]
+      """);
+    System.out.println(p);
   }
 }
